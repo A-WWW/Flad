1ac36a4ae156cfef414fc109bd51b2a9e04d4079
ASGWWWW <ASGWWW@gmail.com>
2022-01-01 22:13:36 +0200

    V4

---
+++ FBD_2.py
@@ -0,0 +1,45 @@
+from flask import Flask,  render_template, url_for, request, flash, session, redirect, abort
+
+
+app = Flask(__name__)
+app.config['SECRET_KEY'] = 'flgkigm530lcgg003njlgf'
+
+menu = [{"name": "Установка", "url": "install-flask"},
+        {"name": "Первое приложение", "url": "first-app"},
+        {"name": "Contact", "url": "contact"}]
+
+@app.route("/")
+def index():
+    print(url_for('index'))
+    return render_template('index.html',title="Про Flask", menu = menu)
+
+@app.route("/contact", methods=["POST", "GET"] )
+def contact():
+    if request.method == 'POST':
+        if len(request.form['username']) > 3:
+            flash('Messages sent', category='success')
+        else:
+            flash('Please enter correct data', category='error')
+
+    print(request.form)
+    return render_template('contact.html', title="Contact", menu=menu)
+
+@app.route("/profile/<username>")
+def profile(username):
+    if 'userLogged' not in session or session['userLogged'] != username:
+        abort(401)
+    return f"User: {username}"
+
+@app.route("/login", methods=["POST", "GET"])
+def login():
+    if 'userLogged' in session:
+        return redirect(url_for('profile', username=session['userLogged']))
+    elif request.form['username'] == "selfedu" and request.form['psw'] == "1111":
+        session['userLogged'] = request.form['username']
+        return redirect(url_for('profile', username=session['userLogged']))
+
+    return render_template('login.html', title="Authorization", menu=menu)
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
